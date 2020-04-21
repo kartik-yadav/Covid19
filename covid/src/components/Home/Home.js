4e4a8d82f5559f62feb7d2665f00340674343e52
kartik-yadav <kartik.yadav@tothenew.com>
2020-04-21 08:57:13 +0530

    Updating Homepage

--- covid/src/components/Home/Home.js
+++ covid/src/components/Home/Home.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./Home.css";
 import Cards from "../Cards/Cards";
 import Recovery from  "../Recovery/Recovery";
-// import Twitter from "../Twitter/Twitter";
+import Twitter from "../Twitter/Twitter";
 const Home = () => {
     return (
         <div className="homecontainer">
             <div className="cards">
                 <Cards/>
             </div>
-            <div className="rcontainer">
+            <div className="rtcontainer">
                 <Recovery/>
+                <Twitter/>
             </div>
         </div>
     );
